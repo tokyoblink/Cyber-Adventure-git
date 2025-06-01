55a4afb19c79d66228f265bd9bc636cab8ecc1f0
Ali <aliyar.aydarov@bk.ru>
2025-06-02 01:16:00 +0300

    Initial commit

---
+++ Assets/Scripts/Hero.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero : MonoBehaviour
+{
+    [SerializeField] private float speed = 3f; // скорость движения
+    [SerializeField] private int lives = 5; // количество жизней
+    [SerializeField] private float jumpForce = 8f; // сила прыжка
+
+    private bool isGrounded = false;
+    private Rigidbody2D rb;
+    private SpriteRenderer sprite;
+
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetButton("Horizontal"))
+            Run();
+        if (Input.GetButtonDown("Jump"))
+            Jump();
+    }
+
+    private void Run()
+    {
+        Vector3 dir = transform.right * Input.GetAxis("Horizontal");
+
+        transform.position = Vector3.MoveTowards(transform.position, transform.position + dir, speed * Time.deltaTime);
+
+        sprite.flipX = dir.x < 0.0f;
+    }
+
+    private void Jump()
+    {
+        rb.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
+    }
+    
+}
