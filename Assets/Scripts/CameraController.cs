2310fc09c3a4a1856a48c5a9f87a5f6f28ac29ed
Ali <aliyar.aydarov@bk.ru>
2025-06-04 17:45:53 +0300

    bugfix + new features
    +added New Animations
    + fix camera contol

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+    private Vector3 pos;
+
+
+
+//Проверка на поиск игрока
+    private void Awake()
+    {
+        if (!player)
+            player = FindObjectOfType<Hero>().transform;
+    }
+
+
+//Поиск игрока при помощи метода лерп 
+    private void Update()
+    {
+        pos = player.position;
+        pos.z = 10f;
+        transform.position = Vector3.Lerp(transform.position, pos, Time.deltaTime);
+    }
+}
--- Assets/Scripts/Hero.cs
+++ Assets/Scripts/Hero.cs
@@ -8,37 +8,85 @@ public class Hero : MonoBehaviour
     [SerializeField] private int lives = 5; // количество жизней
     [SerializeField] private float jumpForce = 8f; // сила прыжка
 
+
     private bool isGrounded = false;
     private Rigidbody2D rb;
+    private Animator anim;
     private SpriteRenderer sprite;
 
+
+    //Анимация
+    private States State
+    {
+        get { return (States)anim.GetInteger("state"); }
+        set { anim.SetInteger("state", (int)value); }
+    }
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponentInChildren<Animator>();
         sprite = GetComponentInChildren<SpriteRenderer>();
+
     }
 
     private void Update()
-    {
-        if (Input.GetButton("Horizontal"))
-            Run();
-        if (Input.GetButtonDown("Jump"))
-            Jump();
+     {
+        if (Input.GetButton("Fire3"))
+        {
+            Sit();
+        }
+        else
+        {
+            if (isGrounded) State = States.idle;
+
+            if (Input.GetButton("Horizontal"))
+                Run();
+
+            if (Input.GetButtonDown("Jump") && isGrounded)
+                Jump();
+        }
     }
 
+
+    // Описание бега
     private void Run()
     {
         Vector3 dir = transform.right * Input.GetAxis("Horizontal");
-
         transform.position = Vector3.MoveTowards(transform.position, transform.position + dir, speed * Time.deltaTime);
-
         sprite.flipX = dir.x < 0.0f;
+
+        if (isGrounded) State = States.run;
     }
 
+
+    // Описание прыжка
     private void Jump()
     {
         rb.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
+        State = States.jump;
+        isGrounded = false;
     }
     
+    // Описание Приседания
+    private void Sit()
+    {
+        State = States.sit;
+    }
+
+    // Проверка на землю (для прыжка)
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+            isGrounded = true;
+    }
+}
+
+//Анимация
+public enum States
+{
+    idle,
+    run,
+    jump,
+    sit
 }
