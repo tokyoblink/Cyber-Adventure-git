ce9405fd77b27719f4545c2babe7a5f58bc3b64a
Ali <aliyar.aydarov@bk.ru>
2025-06-09 23:21:24 +0300

    New Features
    + ui sounds at main menu

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -5,14 +5,33 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+
+    // 1. Ссылка на компонент, который будет проигрывать звук
+    public AudioSource audioSource;
+
+    // 2. Ссылка на сам звуковой эффект (файл)
+    public AudioClip buttonClickSound;
+
+
+
+
+
     public void PlayGame()
     {
+
+        // 3. Проигрываем звук перед загрузкой сцены
+        audioSource.PlayOneShot(buttonClickSound);
+
+
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
 
     public void ExitGame()
     {
+        // 3. Проигрываем звук перед выходом
+        audioSource.PlayOneShot(buttonClickSound);
+        
         Application.Quit();
         Debug.Log("Игра закрылась");
     }
